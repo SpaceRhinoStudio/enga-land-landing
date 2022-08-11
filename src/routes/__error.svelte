aeba8d3fbee02af91a63ac966163d6eec70509e6
clonomaer <clonomaer@gmail.com>
2022-08-11 07:23:04 +0200

    add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,20 @@
+<script context="module" lang="ts">
+  export function load(args: { error: unknown; status: string | number }) {
+    return {
+      props: {
+        message: _.get(args.error, 'message'),
+        status: args.status,
+      },
+    }
+  }
+</script>
+
+<script lang="ts">
+  import ErrorPage from '$lib/shared/ErrorPage.svelte'
+  import _ from 'lodash'
+
+  export let message: string
+  export let status: string | number
+</script>
+
+<ErrorPage {message} {status} />
